1f3269e83f62f8a0e3187f8e8546406f7489da46
Juan <macbook@Macbook-Pro-de-Juan.local>
2023-10-05 15:08:08 -0400

    updated queries

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         <CustomNavbar />
 
         <main className="">{children}</main>
-        <FacebookPixel />
+        {/* <FacebookPixel /> */}
       </body>
     </html>
     // </ThemeProvider>
